e792024696018d68621c8b7abb4b465ceba72ca5
bdshakhawat <mdshakwat.hossain@gmail.com>
2023-08-23 09:37:40 +0600

    styled added

---
+++ app/single-blog/page.js
@@ -0,0 +1,13 @@
+// Hydration problem creates for this
+
+// import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>This is single-blog page</h1>
+        </div>
+    );
+};
+
+export default page;
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// 'use client'
+const Header = () => {
+    return (
+        <div>
+            <h1>This is Header</h1>
+        </div>
+    );
+};
+
+export default Header;
